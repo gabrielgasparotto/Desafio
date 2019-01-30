792cfacc217cb6f49bfa33d39c938d4067576660
Gabriel Gasparotto Souza Cabral <gabrielgasparotto@brq.com>
2019-01-30 18:48:44 -0200

    Update no mapa e preparação para pegar endereço por API

--- app/src/main/java/com/example/desafio/activity/DetalhesActivity.kt
+++ app/src/main/java/com/example/desafio/activity/DetalhesActivity.kt
@@ -1,5 +1,7 @@
 package com.example.desafio.activity
 
+import android.location.Address
+import android.location.Geocoder
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.desafio.R
@@ -8,6 +10,7 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -16,19 +19,35 @@ class DetalhesActivity : AppCompatActivity() {
     lateinit var mapDetalhes: SupportMapFragment
     lateinit var googleMap: GoogleMap
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalhes)
         setSupportActionBar(toolbar)
 
+        //Trocar para endereco que vai ser "Longradouro, bairro"
+        var (latitude, longitude) = transformaLatLong("Praça da Sé, Sé")
+
         mapDetalhes = supportFragmentManager.findFragmentById(R.id.mapDetalhes) as SupportMapFragment
         mapDetalhes.getMapAsync(OnMapReadyCallback {
             googleMap = it
-
-            val latLng = LatLng(13.03, 77.80)
+            val latLng = LatLng(latitude, longitude)
             googleMap.addMarker(MarkerOptions().position(latLng).title("Casa"))
-            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng ,8f))
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng ,15f))
 
         })
+    }
+
+    private fun transformaLatLong(endereco: String): Pair<Double, Double> {
+        var geocoder = Geocoder(this)
+        lateinit var lista: List<Address>
+        var latitude = 0.0;
+        var longitude = 0.0;
+        lista = geocoder.getFromLocationName(endereco, 1)
+        if (lista.size > 0) {
+            latitude = lista.get(0).getLatitude();
+            longitude = lista.get(0).getLongitude();
+        }
+        return Pair(latitude, longitude)
     }
 }
